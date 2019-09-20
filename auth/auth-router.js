6173ca09bb954808bac9c000e400c7dba556656e
yurika sakae <yurika.sun@gmail.com>
2019-09-20 09:52:21 -0700

    login endpoint complete

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -30,7 +30,23 @@ router.post("/register", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  // implement login
+  const { username, password } = req.body;
+  Users.findByUsername(username)
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: "you are logged in",
+          token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid password" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Error logging in user" });
+    });
 });
 
 module.exports = router;

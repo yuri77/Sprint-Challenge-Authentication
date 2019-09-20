3bf3f06d28262e58b3c29963b64d977b390458eb
yurika sakae <yurika.sun@gmail.com>
2019-09-20 10:02:14 -0700

    authentification middleware created

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,25 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || "let it be a secret 43q56",
+      (err, decodedToken) => {
+        if (err) {
+          res.status(401).json({ message: "Token not valid" });
+        } else {
+          req.user = decodedToken;
+          next();
+        }
+      }
+    );
+  } else {
+    res.status(400).json({ message: "No authorization provided" });
+  }
 };

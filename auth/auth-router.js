fcaee9e45ce06766da6a1d17d133a90c8e4feaae
yurika sakae <yurika.sun@gmail.com>
2019-09-20 09:18:50 -0700

    dependencies installed

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,28 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const restrict = require("./authenticate-middleware.js");
 
-router.post('/register', (req, res) => {
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+    id: user.id
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+  return jwt.sign(
+    payload,
+    process.env.JWT_SECRET || "let it be a secret 43q56",
+    options
+  );
+}
+
+router.post("/register", (req, res) => {
   // implement registration
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
